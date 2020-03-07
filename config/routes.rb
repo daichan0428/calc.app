68ff4db11b49050bbb6afbf3572595e81c6c59a4
daichan0428 <daichan0428@hotmail.com>
2020-03-07 17:43:10 +0900

    error

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'calc/:val1/:cal/:val2' => 'calc#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
